37fadd83d80c8c392f4413a083b170486d2c3455
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-08-24 08:28:35 -0700

    Update Q2.py

--- LABS/LAB1/Q2.py
+++ LABS/LAB1/Q2.py
@@ -1,7 +1,8 @@
+operator = input("Enter the operation you want to perform: ")
 n1 = float(input("Enter first number: "))
 n2 = float(input("Enter seconnd number: "))
 n3 = float(input("Enter third number: "))
-operator = input("Enter the operation you want to perform: ")
+
 result = 0
 
 if(operator == "+"):
@@ -14,3 +15,4 @@ elif(operator == "/"):
     result= n1 / n2
 else:
     print("Invalid Operator")
+print("The Result is: " + str(round(result, 1)))
