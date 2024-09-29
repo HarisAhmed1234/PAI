e0f51d75c16d82e84df017eaddb44c1667ea9179
HarisAhmed1234 <143439820+HarisAhmed1234@users.noreply.github.com>
2024-09-29 10:24:32 -0700

    Update Q04.py

--- LABS/LAB6/Q04.py
+++ LABS/LAB6/Q04.py
@@ -1,17 +1,10 @@
 import pandas as pd
 
-data = {
-    'name': ['Alice', 'Bob', 'Charlie', 'David'],
-    'score': [85, 90, 75, 88],
-    'attempts': [1, 2, 1, 2]
-}
+data = {'name': ['Ali', 'Haris', 'Sara', 'Faizan', 'Ayesha'],
+        'age': [25, 22, 28, 24, 26]}
 
 df = pd.DataFrame(data)
 
-print("Original DataFrame:")
-print(df)
+df.index = range(101, 101 + len(df))
 
-df.index = range(100, 100 + len(df))
-
-print("\nDataFrame with custom index starting at 100:")
 print(df)
