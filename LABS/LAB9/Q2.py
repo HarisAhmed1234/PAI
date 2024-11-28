e852bfaab3ea7345357c1bbf67260bb2b1ed1ebe
HarisAhmed1234 <harisahmed537302@gmail.com>
2024-11-28 16:17:59 +0500

    Update Q2.py

--- LABS/LAB9/Q2.py
+++ LABS/LAB9/Q2.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
-cities = ['City A', 'City B', 'City C', 'City D', 'City E']
-populations = [1200000, 850000, 500000, 3000000, 1500000]
+city_names = ['Metropolis A', 'Metropolis B', 'Metropolis C', 'Metropolis D', 'Metropolis E']
+city_populations = [1200000, 850000, 500000, 3000000, 1500000]
 
 plt.figure(figsize=(10, 6))
-plt.barh(cities, populations, color='skyblue')
-plt.title('Population of Cities')
-plt.xlabel('Population')
-plt.ylabel('Cities')
-plt.grid(axis='x')
+plt.barh(city_names, city_populations, color='lightcoral')  
+plt.title('City Populations')
+plt.xlabel('Population Count')
+plt.ylabel('City Names')
+plt.grid(axis='x', linestyle='--')
 
 plt.tight_layout()
 plt.show()
