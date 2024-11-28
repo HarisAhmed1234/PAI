2a0c8d851abfe841343cdb5697dbe4d0b58f57be
HarisAhmed1234 <harisahmed537302@gmail.com>
2024-11-28 16:20:02 +0500

    Update Q3.py

--- LABS/LAB9/Q3.py
+++ LABS/LAB9/Q3.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 
-flavors = ['Vanilla', 'Chocolate', 'Strawberry', 'Mint', 'Cookie Dough']
-scoops_sold = [150, 100, 80, 60, 90]
+ice_cream_flavors = ['Vanilla', 'Chocolate', 'Strawberry', 'Mint', 'Cookie Dough']
+sales_data = [150, 100, 80, 60, 90]
 
+#pie chart
 plt.figure(figsize=(8, 6))
-plt.pie(scoops_sold, labels=flavors, autopct='%1.1f%%', startangle=140)
-plt.title('Distribution of Ice Cream Sales')
-plt.axis('equal')
+plt.pie(sales_data, labels=ice_cream_flavors, autopct='%1.1f%%', startangle=120, colors=['#FFD700', '#8B4513', '#FF69B4', '#98FB98', '#D3D3D3']) 
+plt.title('Ice Cream Sales Distribution')
+plt.axis('equal') 
 plt.show()
